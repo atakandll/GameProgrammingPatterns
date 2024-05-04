5d0896ee5969d1a2b12ce47d1d06ca59ce17c682
atakandll <atakanccnar@outlook.com>
2024-05-04 23:31:25 +0300

    feat(strategy) : Strategy pattern structure logic implemented.

---
+++ Assets/Scripts/StrategyPattern/Structure/Structure.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+namespace StrategyPattern.Structure
+{
+    public class Example
+    {
+        private IStrategy _strategyA = new StrategyA();
+        private IStrategy _strategyB = new StrategyB();
+        
+        private IStrategy CurrentStrategy { get; set; }
+
+        public void ExecuteStrategy()
+        {
+            CurrentStrategy.Execute();
+        }
+
+        public Example()
+        {
+            CurrentStrategy = _strategyA;
+            
+        }
+
+        public void SwapStrategy()
+        {
+            CurrentStrategy = CurrentStrategy == _strategyA ? _strategyB : _strategyA;
+            
+        }
+    }
+    public class Client
+    {
+        private IStrategy strategy { get; }
+        
+        public Client(IStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
+        
+        public void ExecuteStrategy()
+        {
+            strategy.Execute();
+        }
+        
+    }
+
+    public interface IStrategy
+    {
+        void Execute();
+    }
+
+    public class StrategyA : IStrategy
+    {
+        public void Execute()
+        {
+            Debug.Log("Strategy A");
+        }
+    }
+    public class StrategyB : IStrategy
+    {
+        public void Execute()
+        {
+            Debug.Log("Strategy B");
+        }
+    }
+}
