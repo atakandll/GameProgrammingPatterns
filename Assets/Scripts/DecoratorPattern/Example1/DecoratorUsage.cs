55ab705fea109cddba20c0ec757adab85b9fd066
atakandll <atakanccnar@outlook.com>
2024-05-05 21:11:42 +0300

    feat(decorator) : Decorator pattern real life example finished.

--- Assets/Scripts/DecoratorPattern/Example1/DecoratorUsage.cs
+++ Assets/Scripts/DecoratorPattern/Example1/DecoratorUsage.cs
@@ -1,9 +1,97 @@
 ﻿using UnityEngine;
 
-namespace DecaratorPattern.Example1
+namespace DecoratorPattern.Example1
 {
     public class DecoratorUsage : MonoBehaviour
     {
-        
+        private void Start()
+        {
+            IPizza plainPizza = new PlainPizza();
+            PizzaDecorator pizzaCheeseDecorator = new Cheese(plainPizza);
+            PizzaDecorator pizzaOliveDecorator = new Olives(pizzaCheeseDecorator);
+            PizzaDecorator pizzaOliveDecorator2 = new Olives(plainPizza);
+
+            Debug.Log(pizzaOliveDecorator.GetDescription());
+            Debug.Log(pizzaOliveDecorator.GetCost());
+            Debug.Log(pizzaOliveDecorator2.GetCost());
+
+        }
+
+        public interface IPizza
+        {
+            string GetDescription();
+            double GetCost();
+        }
+
+        public class PlainPizza : IPizza
+        {
+            public string GetDescription()
+            {
+                return "Plain Pizza";
+            }
+
+            public double GetCost()
+            {
+                return 10.00;
+            }
+        }
+
+        public abstract class PizzaDecorator : IPizza
+        {
+            protected IPizza _pizza;
+
+            protected PizzaDecorator(IPizza pizza)
+            {
+                _pizza = pizza;
+            }
+
+            public virtual string GetDescription()
+            {
+                return _pizza.GetDescription();
+            }
+
+            public virtual double GetCost()
+            {
+                return _pizza.GetCost();
+            }
+        }
+
+        public class Cheese : PizzaDecorator
+        {
+            public Cheese(IPizza pizza) : base(pizza)
+            {
+                Debug.Log("Adding Cheese");
+            }
+
+            public override string GetDescription()
+            {
+                return $"{_pizza.GetDescription()}, Cheese";
+            }
+
+            public override double GetCost()
+            {
+                return _pizza.GetCost() + 2.00;
+            }
+
+        }
+
+        public class Olives : PizzaDecorator
+        {
+            public Olives(IPizza pizza) : base(pizza)
+            {
+                Debug.Log("Adding Olives");
+            }
+
+            public override string GetDescription()
+            {
+                return $"{_pizza.GetDescription()}, Olives";
+            }
+
+            public override double GetCost()
+            {
+                return _pizza.GetCost() + 3.00;
+            }
+
+        }
     }
 }
