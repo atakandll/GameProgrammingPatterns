03a598f63b0742a46898bf64e87d8f46a0c15e01
atakandll <atakanccnar@outlook.com>
2024-05-14 23:11:42 +0300

    feat(singletonPattern) : PersistenceSingleton class implemented.

---
+++ Assets/Scripts/Singleton/Structure/PersistenceSingleton.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Singleton.Structure
+{
+    public class PersistenceSingleton<T> : MonoBehaviour where T : Component
+    {
+        private static T instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if(instance == null)
+                    CreateInstance();
+                else
+                    HandleDuplication();
+
+                return instance;
+
+            }
+        }
+
+        private static void CreateInstance()
+        {
+            var go = new GameObject(typeof(T).ToString());
+            instance = go.AddComponent<T>();
+        }
+
+        private static void HandleDuplication()
+        {
+            var copies = FindObjectsOfType(typeof(T));
+            
+            foreach (var copy in copies)
+            {
+                
+                if(copy!=instance)
+                    Destroy(copy);
+            }
+        }
+
+    }
+}
