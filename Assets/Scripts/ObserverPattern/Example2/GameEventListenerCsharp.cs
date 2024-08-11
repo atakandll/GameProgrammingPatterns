bb154bb382f2140d379c596644da81ff9dbf92cc
atakandll <atakanccnar@outlook.com>
2024-08-11 05:26:37 +0300

    feat(observer) : New example added for Actions

---
+++ Assets/Scripts/ObserverPattern/Example2/GameEventListenerCsharp.cs
@@ -0,0 +1,52 @@
+﻿using ObserverPattern.Structure;
+using UnityEngine;
+
+namespace ObserverPattern.Example2
+{
+    public class GameEventListenerCsharp : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            ObserverCsharpAction.OnStartGame += OnStartGame;
+            ObserverCsharpAction.OnEndGame += OnEndGame;
+            ObserverCsharpAction.OnPassTurn += OnPassTurn;
+            ObserverCsharpAction.OnEndTurn += OnEndTurn;
+        }
+
+        private void Start()
+        {
+            ObserverCsharpAction.OnEndGame?.Invoke(1,2,true);
+            ObserverCsharpAction.OnPassTurn?.Invoke();
+            ObserverCsharpAction.OnEndTurn?.Invoke();
+            ObserverCsharpAction.OnStartGame?.Invoke();
+        }
+
+        private void OnEndTurn()
+        {
+            Debug.Log("OnEndTurn");
+        }
+
+        private void OnPassTurn()
+        {
+            Debug.Log("OnPassTurn");
+        }
+
+        private void OnEndGame(int arg1, float arg2, bool arg3)
+        {
+            Debug.Log("OnEndGame" + arg1  + arg2 + arg3);
+        }
+
+        private void OnStartGame()
+        {
+            Debug.Log("OnStartGame");
+        }
+
+        private void OnDisable()
+        {
+            ObserverCsharpAction.OnStartGame -= OnStartGame;
+            ObserverCsharpAction.OnEndGame -= OnEndGame;
+            ObserverCsharpAction.OnPassTurn -= OnPassTurn;
+            ObserverCsharpAction.OnEndTurn -= OnEndTurn;
+        }
+    }
+}
---
+++ Assets/Scripts/ObserverPattern/Structure/ObserverCsharpAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace ObserverPattern.Structure
+{
+    public abstract class ObserverCsharpAction
+    {
+        public static Action OnStartGame = delegate { };
+        public static Action<int, float, bool> OnEndGame = delegate{};
+        public static Action OnPassTurn = delegate { };
+        public static Action OnEndTurn = delegate { };
+       
+    }
+}
